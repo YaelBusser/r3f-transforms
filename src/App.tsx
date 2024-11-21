61de160f17ea8a051e58cba485d0f85cac2de7f0
Yaël Busser <mrmaclevine@gmail.com>
2024-11-21 13:08:54 +0100

    transforms course done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import './App.css'
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
+import * as THREE from "three";
 
 function App() {
     return (
         <Canvas camera={{
-            position: [3, 3, 3]
+            position: [0, 0, 7]
         }}>
-            <mesh>
-                <OrbitControls/>
+            <OrbitControls/>
+            <mesh position={[0, 0, 0]} scale={[1, 0.5, 2]}
+                  rotation={[THREE.MathUtils.degToRad(50), THREE.MathUtils.degToRad(10), 0]}>
                 <boxGeometry/>
                 <meshNormalMaterial/>
             </mesh>
+            <mesh position={[2, 0, 0]} scale={[1.5, 1, 1.5]} rotation={[0, THREE.MathUtils.degToRad(150), 0]}>
+                <boxGeometry/>
+                <meshNormalMaterial/>
+            </mesh>
+            <mesh position={[-2, 0, 0]} scale={[0.5, 1.5, 0.5]} rotation={[0, 0, THREE.MathUtils.degToRad(50)]}>
+                <boxGeometry/>
+                <meshNormalMaterial/>
+            </mesh>
+            <group position={[0, 2, 0]} scale={1.5} rotation-z={THREE.MathUtils.degToRad(5)}>
+                <mesh>
+                    <boxGeometry/>
+                    <meshNormalMaterial/>
+                </mesh>
+                <mesh position-x={-2}>
+                    <boxGeometry/>
+                    <meshNormalMaterial/>
+                </mesh>
+                <mesh position-x={2}>
+                    <boxGeometry/>
+                    <meshNormalMaterial/>
+                </mesh>
+            </group>
         </Canvas>
     )
 }
